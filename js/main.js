fe4982b44f41ed8d1a720916e018666ed8be51e5
SirHacksalot1337 <jsjohnson@wpi.edu>
2022-04-15 22:37:33 -0400

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // main.js
 
-const { default: Moralis } = require("moralis/types");
+import { default as Moralis } from "moralis/types";
 
 const serverUrl = "https://ilfapq8h4jyq.usemoralis.com:2053/server";
 const appId = "C2fy4GQRDTtKZpuTPEElzLXFObQMotGnvt7uFMT2";
