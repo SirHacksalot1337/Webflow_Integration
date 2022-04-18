cbfd2301b7311789a6140d70d6f46433f44d93d6
SirHacksalot1337 <jsjohnson@wpi.edu>
2022-04-18 15:56:30 -0400

    asdf

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ const serverUrl = "https://ilfapq8h4jyq.usemoralis.com:2053/server";
 const appId = "C2fy4GQRDTtKZpuTPEElzLXFObQMotGnvt7uFMT2";
 const contract = "0xd203058935aa3Bac85AA4a6abF5732c5d0ebf74D"
 const stakeContract = "0xaf9a036655bfE05ab653B57C16c9983c944CF43B"
-const tokenContract = "0xe8Be2Ff20b3eefD7f068ca9e94808C036F283452"
+//const tokenContract = "0xe8Be2Ff20b3eefD7f068ca9e94808C036F283452"
 const chain = "avalanche testnet"
 
 
@@ -143,4 +143,4 @@ async function stake() {
 
 cashedSession();
 
-document.getElementById("btn-expedition").onclick = stake;
+//document.getElementById("btn-expedition").onclick = stake;
